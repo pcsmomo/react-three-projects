6d4b7ba0b3e6158a191102793ae98c91ad093edd
Noah Kim <dongwoo_333@hanmail.net>
2023-04-12 19:49:56 +1000

    feat(about): services with react-tilt

--- 3d-portfolio/src/components/About.tsx
+++ 3d-portfolio/src/components/About.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import Tilt from "react-tilt";
+import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -12,7 +12,28 @@ interface ServiceCardProps {
   icon: string;
 }
 const ServiceCard: FC<ServiceCardProps> = ({ title, index, icon }) => {
-  return <p>{title}</p>;
+  return (
+    <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+      className="xs:w-[250px] w-full"
+    >
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
 };
 
 const About = () => {
@@ -24,7 +45,7 @@ const About = () => {
       </motion.div>
 
       <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
+        variants={fadeIn("up", "tween", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
         My name is Noah, and I am a skilled frontend developer with a degree in
