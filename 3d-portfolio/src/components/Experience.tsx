b7076802e1618718a0c594f81d08589e26801e56
Noah Kim <dongwoo_333@hanmail.net>
2023-04-18 22:21:50 +1000

    feat(work): add projects with Tilt card

--- 3d-portfolio/src/components/Experience.tsx
+++ 3d-portfolio/src/components/Experience.tsx
@@ -68,7 +68,7 @@ const Experience = () => {
     <>
       <motion.div variants={textVariant(0.1)}>
         <p className={styles.sectionSubText}>What I have done so far</p>
-        <p className={styles.sectionHeadText}>Work Experience.</p>
+        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
       </motion.div>
 
       <div className="mt-20 flex flex-col">
