3cc72c4b1e3f4d6645299b050306cc9bd8e04750
Noah Kim <dongwoo_333@hanmail.net>
2023-04-22 18:46:10 +1000

    feat(stars): add stars effect with @react-three

--- 3d-portfolio/src/components/canvas/Stars.tsx
+++ 3d-portfolio/src/components/canvas/Stars.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { useMemo, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import * as random from "maath/random/dist/maath-random.cjs";
+
+// for typing: https://docs.pmnd.rs/react-three-fiber/tutorials/typescript
+import { Points as ThreePoints } from "three";
 
 const Stars = () => {
-  return <div>Stars</div>;
+  const ref = useRef<ThreePoints>(null);
+  const sphere = useMemo(
+    () =>
+      random.inSphere(new Float32Array(5000), {
+        radius: 1.2,
+      }) as Float32Array,
+    []
+  );
+
+  useFrame((_state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10;
+      ref.current.rotation.y -= delta / 15;
+    }
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled>
+        <PointMaterial
+          transparent
+          color="#f272c8"
+          size={0.002}
+          sizeAttenuation={true}
+          deptWrite={false}
+        />
+      </Points>
+    </group>
+  );
 };
 
-export default Stars;
+const StarsCanvas = () => {
+  return (
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </div>
+  );
+};
+
+export default StarsCanvas;
