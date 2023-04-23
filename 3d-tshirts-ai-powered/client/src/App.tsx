9435c36bfad01b9f30ec1d415956aa3eef8c43a4
Noah Kim <dongwoo_333@hanmail.net>
2023-04-23 17:02:09 +1000

    chore: create skeleton components

--- 3d-tshirts-ai-powered/client/src/App.tsx
+++ 3d-tshirts-ai-powered/client/src/App.tsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import Home from "./pages/Home";
+import Customizer from "./pages/Customizer";
+import Canvas from "./canvas";
 
 function App() {
-  return <>App</>;
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <Customizer />
+      <Canvas />
+    </main>
+  );
 }
 
 export default App;
