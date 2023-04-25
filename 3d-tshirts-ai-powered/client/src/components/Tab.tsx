51de97bdb50f945a96cee9a3fcad01c055ad11b0
Noah Kim <dongwoo_333@hanmail.net>
2023-04-25 21:37:39 +1000

    feat(tab): tab component and styling

--- 3d-tshirts-ai-powered/client/src/components/Tab.tsx
+++ 3d-tshirts-ai-powered/client/src/components/Tab.tsx
@@ -1,5 +1,50 @@
-const Tab = () => {
-  return <div>Tab</div>;
+import { FC } from "react";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+
+type TabType = {
+  name: string;
+  icon: string;
+};
+
+interface TabProps {
+  tab: TabType;
+  isFilterTab?: boolean;
+  isActiveTab?: boolean;
+  handleClick: () => void;
+}
+
+const Tab: FC<TabProps> = ({
+  tab,
+  isFilterTab = false,
+  isActiveTab = false,
+  handleClick,
+}) => {
+  const snap = useSnapshot(state);
+
+  const activeStyles =
+    isFilterTab && isActiveTab
+      ? { backgroundColor: snap.color, opacity: 0.5 }
+      : { backgroundColor: "transparent", opacity: 1 };
+
+  return (
+    <div
+      key={tab.name}
+      className={`tab-btn ${
+        isFilterTab ? "rounded-full glassmorphism" : "rounded-4"
+      }`}
+      onClick={handleClick}
+      style={activeStyles}
+    >
+      <img
+        src={tab.icon}
+        className={`${
+          isFilterTab ? "w-2/3 h-2/3" : "w-11/12 h-11/12 object-contain"
+        }`}
+      />
+    </div>
+  );
 };
 
 export default Tab;
