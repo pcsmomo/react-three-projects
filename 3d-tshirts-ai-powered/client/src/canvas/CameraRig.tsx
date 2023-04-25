8dca1c81d8ea29f32bd37630bfc96a7714f1810e
Noah Kim <dongwoo_333@hanmail.net>
2023-04-25 19:16:08 +1000

    feat(camera): responsive camera position setup and rotate

--- 3d-tshirts-ai-powered/client/src/canvas/CameraRig.tsx
+++ 3d-tshirts-ai-powered/client/src/canvas/CameraRig.tsx
@@ -1,5 +1,48 @@
-const CameraRig = () => {
-  return <div>CameraRig</div>;
+import React, { FC, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+import { Group } from "three";
+
+import storeState from "../store";
+
+interface CameraRigProps {
+  children: React.ReactNode;
+}
+
+const CameraRig: FC<CameraRigProps> = ({ children }) => {
+  const group = useRef<Group>(null);
+  const snap = useSnapshot(storeState);
+
+  useFrame((state, delta) => {
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+
+    // set the initial position of the model
+    let targetPosition: [x: number, y: number, z: number] = [-0.4, 0, 2];
+    if (snap.intro) {
+      if (isBreakpoint) targetPosition = [0, 0, 2];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
+    } else {
+      if (isMobile) targetPosition = [0, 0, 2.5];
+      else targetPosition = [0, 0, 2];
+    }
+
+    // set model camera position
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+
+    // set the model rotation smoothly
+    if (group.current) {
+      easing.dampE(
+        group.current.rotation,
+        [state.pointer.y / 10, -state.pointer.x / 5, 0],
+        0.25,
+        delta
+      );
+    }
+  });
+
+  return <group ref={group}>{children}</group>;
 };
 
 export default CameraRig;
