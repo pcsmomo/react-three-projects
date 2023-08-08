d4fd13318593f10fd96dde3b3af878ca8c7ce4b2
Noah Kim <dongwoo_333@hanmail.net>
2023-08-08 17:19:13 +1000

    16. AI Backend - openai

--- 3d-tshirts-ai-powered/server/src/index.ts
+++ 3d-tshirts-ai-powered/server/src/index.ts
@@ -2,12 +2,16 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+import dalleRoutes from "./routes/dalle.routes";
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello from DALL.E" });
